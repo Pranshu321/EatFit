d7c1e30fd773db1b3614c3fe6f935ee77595f492
Mohammad Sami <mohammadsami@duck.com>
2023-10-29 13:14:47 +0530

    ADD: upload photo

--- Frontend/src/components/dashboard/CalorieSnap.jsx
+++ Frontend/src/components/dashboard/CalorieSnap.jsx
@@ -5,10 +5,10 @@ import { auth, db } from "../../firebase/firebase";
 
 function CalorieSnap() {
 	const [photo, setPhoto] = useState(null);
-    const navigate = useNavigate();
+	const [file, setFile] = useState();
+	const navigate = useNavigate();
 
-
-    useEffect(() => {
+	useEffect(() => {
 		onAuthStateChanged(auth, (user) => {
 			if (user) {
 				// User is signed in, see docs for a list of available properties
@@ -25,11 +25,33 @@ function CalorieSnap() {
 
 	const handlePhotoChange = (event) => {
 		setPhoto(event.target.files[0]);
+		setFile(URL.createObjectURL(event.target.files[0]));
 	};
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		// Do something with the photo, such as upload it to a server
+		var data = new FormData();
+		var imagedata = document.querySelector('input[type="file"]').files[0];
+		data.append("data", imagedata);
+		data.append("data", imagedata);
+
+		fetch("http://localhost:8000/upload/", {
+			mode: "no-cors",
+			method: "POST",
+			body: data,
+		}).then(
+			function (res) {
+				if (res.ok) {
+					alert("Perfect! ");
+				} else if (res.status == 401) {
+					alert("Oops! ");
+				}
+			},
+			function (e) {
+				alert("Error submitting form!");
+			}
+		);
 	};
 
 	return (
@@ -160,42 +182,47 @@ function CalorieSnap() {
 							className="flex flex-col items-center justify-center w-full"
 						>
 							<div class="flex items-center justify-center w-full py-20">
-								<label
-									for="dropzone-file"
-									class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-								>
-									<div class="flex flex-col items-center justify-center pt-5 pb-6">
-										<svg
-											class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-											aria-hidden="true"
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 20 16"
-										>
-											<path
-												stroke="currentColor"
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
-												d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-											/>
-										</svg>
-										<p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-											<span class="font-semibold">
-												Click to upload
-											</span>
-											or drag and drop
-										</p>
-										<p class="text-xs text-gray-500 dark:text-gray-400">
-											SVG, PNG, JPG or GIF (MAX. 800x400px)
-										</p>
-									</div>
-									<input
-										id="dropzone-file"
-										type="file"
-										class="hidden"
-									/>
-								</label>
+								{!file ? (
+									<label
+										for="dropzone-file"
+										class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+									>
+										<div class="flex flex-col items-center justify-center pt-5 pb-6">
+											<svg
+												class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+												aria-hidden="true"
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 20 16"
+											>
+												<path
+													stroke="currentColor"
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													stroke-width="2"
+													d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+												/>
+											</svg>
+											<p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+												<span class="font-semibold">
+													Click to upload
+												</span>
+												or drag and drop
+											</p>
+											<p class="text-xs text-gray-500 dark:text-gray-400">
+												SVG, PNG, JPG or GIF (MAX. 800x400px)
+											</p>
+										</div>
+										<input
+											id="dropzone-file"
+											type="file"
+											class="hidden"
+											onChange={handlePhotoChange}
+										/>
+									</label>
+								) : (
+									<img src={file} />
+								)}
 							</div>
 
 							<button
